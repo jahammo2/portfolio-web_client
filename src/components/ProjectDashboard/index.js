4fc86c81a10c6ac9e2bf55f5eff6f08c826fd693
Jordan Hammond <jahammo2@gmail.com>
2016-07-22 16:56:32 -0400

    added in scrolling refactor

--- src/components/ProjectDashboard/index.js
+++ src/components/ProjectDashboard/index.js
@@ -2,16 +2,40 @@ import React, { Component, PropTypes } from 'react';
 import { Map, List } from 'immutable';
 import './index.scss';
 import Info from './Info';
-import { getColor } from '../../utils/ProjectHelpers';
+import { getColor, getSisterProject } from '../../utils/ProjectHelpers';
 import DeviceImage from '../DeviceImage';
 
 const propTypes = {
   project: PropTypes.instanceOf(Map),
+  setActiveProject: PropTypes.func,
+  projects: PropTypes.instanceOf(List),
   colorSets: PropTypes.instanceOf(List),
   children: PropTypes.node
 };
 
-class ProjectDashboard extends Component {
+export class ProjectDashboard extends Component {
+  constructor () {
+    super();
+    this.state = { currentlyChanging: false };
+  }
+
+  scrollThroughProjects (e) {
+    // Scrolling up
+    if (e.deltaY === -18 && this.state.currentlyChanging === false) {
+      const sisterProject = getSisterProject(1, this.props.projects, this.props.project);
+      this.setState({currentlyChanging: true});
+      this.props.setActiveProject(sisterProject);
+    // Scrolling down
+    } else if (e.deltaY === 18 && this.state.currentlyChanging === false) {
+      const sisterProject = getSisterProject(-1, this.props.projects, this.props.project);
+      this.setState({currentlyChanging: true});
+      this.props.setActiveProject(sisterProject);
+    // Scrolling nowhere
+    } else if (e.deltaY === -0) {
+      this.setState({currentlyChanging: false});
+    }
+  }
+
   projectDashboardStyles () {
     const color = getColor(
       this.props.project,
@@ -29,6 +53,7 @@ class ProjectDashboard extends Component {
       <div
         className='project-dashboard'
         style={this.projectDashboardStyles()}
+        onWheel={(e) => {this.scrollThroughProjects(e);}}
       >
         <div className='project-dashboard__container'>
           <div className='project-dashboard__image'>
