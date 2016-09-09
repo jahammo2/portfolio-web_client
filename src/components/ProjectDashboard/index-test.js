40b9f878581113a367ea452348b9ff9952535bff
Jordan Hammond <jahammo2@gmail.com>
2016-09-09 17:35:29 -0400

    added tests and linted

--- src/components/ProjectDashboard/index-test.js
+++ src/components/ProjectDashboard/index-test.js
@@ -2,16 +2,14 @@ import { ProjectDashboard } from './index';
 import * as ProjectHelpers from '../../utils/ProjectHelpers';
 
 describe('ProjectDashboard', () => {
-  describe('scrollThroughProjects', () => {
+  describe('swipe', () => {
     let projectDashboard;
     let getSisterProject;
-    let setState;
     let setActiveProject;
+    let direction;
 
     before(() => {
       projectDashboard = new ProjectDashboard();
-      projectDashboard.state = { currentlyChanging: false };
-
       projectDashboard.props = {
         projects: new List([]),
         project: new Map(),
@@ -19,6 +17,26 @@ describe('ProjectDashboard', () => {
           return;
         }
       };
+      direction = 1;
+    });
+
+    beforeEach(() => {
+      getSisterProject = spy(ProjectHelpers, 'getSisterProject');
+      setActiveProject = spy(projectDashboard.props, 'setActiveProject');
+      projectDashboard.swipe(direction);
+    });
+
+    it('calls getSisterProject', () => {
+      expect(getSisterProject.calledWith(direction)).to.be.true;
+    });
+
+    it('calls setActiveProject', () => {
+      expect(setActiveProject.calledOnce).to.be.true;
+    });
+
+    afterEach(() => {
+      getSisterProject.restore();
+      setActiveProject.restore();
     });
   });
 });
