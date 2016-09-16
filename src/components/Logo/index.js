477825d6bde4a20f819d042df79d4c06c858d55f
Jordan Hammond <jahammo2@gmail.com>
2016-09-16 17:07:10 -0400

    added new colors to logo

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -10,29 +10,30 @@ const propTypes = {
 };
 
 class Logo extends Component {
-  styles () {
+  styles (name) {
     const color = getColor(
       this.props.project,
       this.props.colorSets,
-      'button'
+      name
     );
 
-    return {
-      background: color
-    };
+    return color;
   }
 
   render () {
     return (
       <div
         className='logo column-between'
-        style={this.styles()}
+        style={{ background: this.styles('logo_background') }}
       >
         <img
           className='logo__image'
           src={this.props.logo}
         />
-        <p className='logo__title'>{this.props.project.getIn(['attributes', 'title'])}</p>
+        <p
+          className='logo__title'
+          style={{ color: this.styles('logo_title') }}
+        >{this.props.project.getIn(['attributes', 'title'])}</p>
       </div>
     );
   }
