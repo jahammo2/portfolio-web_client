b8462b1c8153b20d530f478431f45589d1bf33f2
Jordan Hammond <jahammo2@gmail.com>
2016-10-19 18:46:06 -0400

    testing milliseconds

--- src/components/DeviceImage/index.js
+++ src/components/DeviceImage/index.js
@@ -23,7 +23,7 @@ class DeviceImage extends Component {
       // the timeout gives a little extra time for the laptop or mobile image to load in
       setTimeout(() => {
         this.setState({ showing: props.device });
-      }, 50);
+      }, 200);
     }
   }
 
