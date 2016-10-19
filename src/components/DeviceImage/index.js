3ced96448e8889863b67ae7be66d3f0688a2ba44
Jordan Hammond <jahammo2@gmail.com>
2016-10-19 18:42:45 -0400

    switched to WillMount

--- src/components/DeviceImage/index.js
+++ src/components/DeviceImage/index.js
@@ -14,12 +14,11 @@ class DeviceImage extends Component {
     this.state = { showing: null };
   }
 
-  componentDidMount () {
+  componentWillMount () {
     this.setState({ showing: this.props.device });
   }
 
   componentWillReceiveProps (props) {
-    console.log(props);
     if (props.device !== this.state.showing) {
       // the timeout gives a little extra time for the laptop or mobile image to load in
       setTimeout(() => {
@@ -39,8 +38,6 @@ class DeviceImage extends Component {
   render () {
     const device = this.props.device;
 
-    // console.log(this.state.showing);
-    // console.log(device);
     if (this.state.showing === device) {
       return (
         <div className={`device-image device-image--${device}`}>
