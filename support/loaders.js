1c58edfeefcae50bedc22ad3374e41e405fcd3a1
Jordan Hammond <jahammo2@gmail.com>
2019-01-05 14:43:11 -0500

    removed unnecessary file

--- support/loaders.js
+++
@@ -1,5 +0,0 @@
-// const nullExtensions = ['.css', '.scss', '.sass', '.png', '.jpg', '.svg'];
-//
-// nullExtensions.forEach(function(extension) {
-//   require.extensions[extension] = function() {};
-// });
